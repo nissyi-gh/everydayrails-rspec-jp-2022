8425e332f4627940c3b73714b8dea770f7fa9eb3
nissyi <yuta.onishi@pepabo.com>
2022-08-17 16:34:31 +0900

    Userへのモデルスペックを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '姓、名、メール、パスワードがあれば有効な状態であること' do
+    user = User.new(
+      first_name: 'Aaron',
+      last_name: 'Summer',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze',
+    )
+    expect(user).to be_valid
+  end
+
+  it '名がなければ無効な状態であること' do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+  
+  it '姓がなければ無効な状態であること' do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+  
+  it 'メールアドレスがなければ無効な状態であること' do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it '重複したメールアドレスなら無効な状態であること' do
+    User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze',
+    )
+    user = User.new(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze',
+    )
+    
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it 'ユーザーのフルネームを文字列として返すこと' do
+    user = User.new(
+      first_name: 'Aaron',
+      last_name: 'Summer',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze',
+    )
+
+    expect(user.name).to eq 'Aaron Summer'
+  end
 end
